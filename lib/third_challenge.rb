eed6d935c040a48951ce353f3dd1ea76e6c8aa4c
Keenan Jones <keenanjones210@gmail.com>
2019-12-25 04:09:20 +0000

    Automatically backed up by Learn

---
+++ lib/third_challenge.rb
@@ -0,0 +1,20 @@
+ third_challenge
+  epic_tragedy = {
+    :montague => {
+      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      }, 
+    :capulet => {
+      #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+  }
+
+  
+end
